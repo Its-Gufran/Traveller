56dd3baff66846e31209b9167c2b40654a93a533
iamgufran <mgfzd786@gmail.com>
2023-05-01 23:06:48 +0530

    Connect Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import './App.css';
+import Home from './Routes/Home'
+import About from './Routes/About'
+import Service from './Routes/Service'
+import Contact from './Routes/Contact'
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/service" element={<Service/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+
+    </Routes>
       <Navbar/>
     </div>
   );
